61ae97170af4676d39e970661fa5675b91d5714f
joelee2017 <as955133@gmail.com>
2017-12-13 12:42:44 +0800

    push

--- 認識Lmabda/Form1.Designer.cs
+++ 認識Lmabda/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.btnodd = new System.Windows.Forms.Button();
+            this.btnCreateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "圓面積";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnCircleArea_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(163, 207);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // btnodd
+            // 
+            this.btnodd.Location = new System.Drawing.Point(163, 274);
+            this.btnodd.Name = "btnodd";
+            this.btnodd.Size = new System.Drawing.Size(75, 23);
+            this.btnodd.TabIndex = 2;
+            this.btnodd.Text = "奇數";
+            this.btnodd.UseVisualStyleBackColor = true;
+            this.btnodd.Click += new System.EventHandler(this.btnodd_Click);
+            // 
+            // btnCreateButton
+            // 
+            this.btnCreateButton.Location = new System.Drawing.Point(163, 144);
+            this.btnCreateButton.Name = "btnCreateButton";
+            this.btnCreateButton.Size = new System.Drawing.Size(75, 23);
+            this.btnCreateButton.TabIndex = 3;
+            this.btnCreateButton.Text = "新button";
+            this.btnCreateButton.UseVisualStyleBackColor = true;
+            this.btnCreateButton.Click += new System.EventHandler(this.btnCreateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 391);
+            this.Controls.Add(this.btnCreateButton);
+            this.Controls.Add(this.btnodd);
+            this.Controls.Add(this.btnDownload);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnodd;
+        private System.Windows.Forms.Button btnCreateButton;
     }
 }
 
--- 認識Lmabda/Form1.cs
+++ 認識Lmabda/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +13,79 @@ namespace 認識Lmabda
 {
     public partial class Form1 : Form
     {
+        public delegate double AreaDelegate(double r);//委派
+        public delegate double AreaDelegate2(double w, double l);//委派
+
         public Form1()
         {
             InitializeComponent();
         }
+
+
+        double AreaCircle(double r)//呼叫方法
+        {
+            return r * r * Math.PI;
+        }
+
+
+        double CustomCircleArea(double r,AreaDelegate ad)//呼叫方法
+        {
+            return ad(r);
+        }
+
+        private void btnCircleArea_Click(object sender, EventArgs e)//lambda運算式
+        {
+            AreaDelegate CircleArea = r => r * r * Math.PI;
+
+            MessageBox.Show($"圓面積：{CircleArea(5)}");
+            MessageBox.Show($"圓面積：{AreaCircle(10)}");
+
+            MessageBox.Show($"圓面積：{CustomCircleArea(5, r => r * r * Math.PI)}");
+            MessageBox.Show($"圓面積：{CustomCircleArea(5, r => (r-0.1) * (r-0.1) * Math.PI)}");
+
+
+            //AreaDelegate SqyareArea = s => s * s;
+            //MessageBox.Show($"正方形面積：{SqyareArea(5)}");
+
+
+            //AreaDelegate2 RectangleArea = (w, l) => w * l;
+            //MessageBox.Show($"正方形面積：{RectangleArea(5,10)}");
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)//lambda 檔案下載
+        {
+            WebClient obj = new WebClient();//NEW一個WebClient物件要 NameSpace
+            //DownloadFileCompleted檔案下載完成時事件
+            obj.DownloadFileCompleted += ( dssender,de)=>
+            {
+                MessageBox.Show("檔案下載完成!");
+            };
+
+
+            obj.DownloadFileAsync(new Uri("http://localhost/iisstart.png"), "image9.png");
+            //DownloadFileAsync下載資料
+            //(new Uri("http://localhost/iisstart.png"), "image9.png");
+            //新增一個路徑/ 下載後檔案的名稱
+        }
+
+        private void btnodd_Click(object sender, EventArgs e)
+        {
+            int[] arr = { 11, 24, 65, 43, 112 };
+            int OddCount = arr.Count(n => n % 2 == 1);
+            MessageBox.Show($"{OddCount} 個奇數");
+        }
+
+        private void btnCreateButton_Click(object sender, EventArgs e)
+        {
+            Button btn = new Button();//新增按金
+            btn.Text = "MSIT117";
+            btn.Click += (dssender, de)=>
+            {
+                MessageBox.Show("hello MSIT117 !");
+            };
+            Controls.Add(btn);
+        }
+
+        
     }
 }
